e4e5f29ca080218cc295be175b889376b0543552
Dharam Dhurandhar <dharamdhurandhar@Dharams-MacBook-Pro.local>
2024-01-14 13:56:38 +0530

    Initial Commit

---
+++ chatbot/chatbotApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatbotApp.swift
+//  chatbot
+//
+//  Created by Dharam Dhurandhar on 14/01/24.
+//
+
+import SwiftUI
+
+@main
+struct chatbotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
